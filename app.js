273249d6fc532b21bed8ac6730be9c316370614a
Herkahahaha <herkahahaha@gmail.com>
2019-09-22 09:12:09 +0700

    reinstall npm

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+/* ------------------
+        CORS 
+---------------------*/
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 const { buildSchema } = require("graphql");
 const graphqlHttp = require("express-graphql");
 // import event from models folder to parsing event input data
 const Event = require("./models/event");
-
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 app.use(
   "/graphql",
   graphqlHttp({
@@ -26,12 +43,23 @@ app.use(
         date: String!
     }
 
+    type User {
+        _id: ID!
+        email: String!
+        password: String
+    }
+    input UserInput {
+        email: String!
+        password: String!
+    }
+
     type RootQuery {
         events: [Event!]!
     }
 
     type RootMutation {
         createEvent(eventInput: EventInput):Event
+        createUser(userInput: UserInput):User
     }
     schema {
         query: RootQuery
@@ -43,7 +71,7 @@ app.use(
         return Event.find()
           .then(events => {
             return events.map(event => {
-              return { ...event._doc, _id: event.doc._id.toString() };
+              return { ...event._doc, _id: event._doc._id.toString() };
             });
           })
           .catch(err => {
@@ -71,11 +99,33 @@ app.use(
           });
       }
     },
+    createUser: args => {
+      return bcrypt
+        .hash(args.userInput.password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+          return user.save();
+        })
+        .then(result => {
+          console.log(result);
+          return {
+            ...result._doc,
+            password: null,
+            _id: result._id
+          };
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     graphiql: true
   })
 );
 
-// connect to monggodb
+/* connect to monggodb*/
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/react-booking-app", {
