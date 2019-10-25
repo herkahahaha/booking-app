ffdb8542003ea90543b32672ab62e9654207e26f
Herkahahaha <herkahahaha@gmail.com>
2019-10-26 01:32:32 +0700

    add Auth

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,22 +6,18 @@ const Navbar = props => {
   return (
     <header className="main-nav">
       <div className="nav-logo">
-        {" "}
-        <h1>Booking App</h1>{" "}
+        <h1>Booking App</h1>
       </div>
       <nav className="nav-item">
         <ul>
           <li>
-            {" "}
-            <NavLink to="">Authenticated</NavLink>{" "}
+            <NavLink to="">Authenticated</NavLink>
           </li>
           <li>
-            {" "}
-            <NavLink to="">Events</NavLink>{" "}
+            <NavLink to="">Events</NavLink>
           </li>
           <li>
-            {" "}
-            <NavLink to="">Booking</NavLink>{" "}
+            <NavLink to="">Booking</NavLink>
           </li>
         </ul>
       </nav>
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Auth = () => {
-  return (
-    <div>
-      <h1>From auth</h1>
-    </div>
-  );
-};
+class Auth extends Component {
+  state = {
+    isLogin: true
+  };
+  constructor(props) {
+    super(props);
+    this.emailRef = React.createRef();
+    this.passwordRef = React.createRef();
+  }
+
+  switchHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDevault();
+    const email = this.emailRef.current.value;
+    const password = this.passwordRef.current.value;
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    console.log(email, password);
+    let requestBody = {
+      query: `
+        query{
+          login(email:"${email}",password:"${password}"){
+            userId
+            token
+            tokenExpiration
+          }
+        }      
+      `
+    };
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+            mutation{
+              createUser(userInput:{email:"${email}",password:"${password}"}){
+                _id
+                email
+              }
+            }      
+          `
+      };
+    }
+    fetch("http://localhost:5000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed catch data");
+        }
+      })
+      .then(resData => console.log(resData))
+      .catch(err => console.log(err));
+  };
+  render() {
+    return (
+      <div>
+        <form className="auth-form" onSubmit={this.handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" ref={this.emailRef} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" ref={this.passwordRef} />
+          </div>
+          <div className="form-action">
+            <button type="submit">Submit</button>
+            <button type="button" onClick={this.switchHandler}>
+              Switch to {this.state.isLogin ? "SignUp" : "Login"}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Auth;
